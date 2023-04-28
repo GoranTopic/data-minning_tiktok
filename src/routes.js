600df3ce613096ea6525322604441e4dcea206a5
Goran Topic <telegonicaxx@live.com>
2023-04-27 20:14:14 -0700

    changed to use list of file

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { write_binary, write_binstr, write_json, read_json, file_not_exists } from 'files-js';
+import { write_binary, write_binstr, write_json, read_json, file_not_exists, file_exists } from 'files-js';
 import fs from 'fs';
 /* 
  * this file is used to set the routers for the page
@@ -16,18 +16,22 @@ let setRoutes = async page => {
     await page.route( '**/*', saveTraffic);
 }
 
-// make a directory of videos with url mapped to request id
-// if file does not exist, make it
-if( file_not_exists('./storage/videos_table.js') ) 
-    write_json({}, './storage/videos_table.js');
-let videos_table = read_json('./storage/videos_table.js');
+let directories = [ 'videos', 'images', 'html', 'json', 'js' ];
 
 // if the directories do not exist, make them
-[ 'videos', 'images', 'html', 'json', 'js' ].forEach( dir => {
+directories.forEach( dir => {
     if( file_not_exists('./storage/' + dir) ) 
         fs.mkdirSync('./storage/' + dir);
 });
 
+// make an obj to map every file with it url
+let tables = {}
+directories.forEach( dir => {
+    // if file does not exist, make it
+    if( file_not_exists(`./storage/${dir}_table.js`) )
+        write_json({}, `./storage/${dir}_table.js`);
+    tables[dir] = read_json(`./storage/${dir}_table.js`);
+});
 
 // abort all requests to images 
 const abortImageRequest = route => 
@@ -67,9 +71,22 @@ const handleVideo = async (route, response, url) => {
     //  get the headers
     let headers = response.headers();
     //  get the request id
-    let request_id = headers['x-storagegw-request-id'];
+    let request_id = headers['x-tt-logid'];
     // add the url to the request id in the videos table
-    videos_table[request_id] = url;
+    // request_id is undefined, throw error
+    if( !request_id ){
+        console.log('headers: ', headers);
+        throw new Error('request id is undefined');
+    }
+    // if file exists, throw error
+    if( file_exists(`./storage/videos/${request_id}`) ){
+        //console.error('headers: ', headers);
+        //throw new Error('video file already exists');
+        console.error('video file already exists');
+        // add the data to the request id in the videos table
+        request_id = `${request_id}_${Date.now()}`;
+    } else // add the url to the request id in the videos table
+        tables['videos'][request_id] = url;
     // get the video
     let video = await response.body();
     // decode the video
@@ -77,7 +94,7 @@ const handleVideo = async (route, response, url) => {
     // save the video
     write_binary(video, `./storage/videos/${request_id}`);
     // save the videos table
-    write_json(videos_table, './storage/videos_table.js');
+    write_json(tables['videos'], './storage/videos_table.js');
     console.log(`saved ./storage/videos/${request_id}`);
 }
 
@@ -86,53 +103,115 @@ const handleImage = async (route, response, url) => {
     //  get the headers
     let headers = response.headers();
     //  get the request id
-    let request_id = headers['x-storagegw-request-id'];
+    let request_id = headers['x-tt-logid'];
+    // if file exists, throw error
+    if( file_exists(`./storage/images/${request_id}`) ){
+        console.error('image file already exists');
+        // add the data to the request id in the images table
+        request_id = `${request_id}_${Date.now()}`;
+    }
+    //  if request id is undefined, throw error
+    if( !request_id ){
+        console.log('headers: ', headers);
+        throw new Error('request id is undefined');
+    } else // add the url to the request id in the images table
+        tables['images'][request_id] = url;
     // get the image
     let image = await response.body();
     // decode the video
     image = Buffer.from(image, 'base64');
-    // make the directories
-    let file_path = _makeDirs(url, './storage/images/');
     // save the video
-    write_binary(image, `./storage/images/${file_path}`);
-    console.log(`saved ./storage/images/${file_path}`);
+    write_binary(image, `./storage/images/${request_id}`);
+    // save the images table
+    write_json(tables['images'], './storage/images_table.js');
+    console.log(`saved ./storage/images/${request_id}`);
 }
 
 
 // handle json files
 const handleJson = async (route, response, url) => {
     //  get the headers
+    let headers = response.headers();
+    //  get the request id
+    let request_id = headers['x-tt-logid'];
+    // if file exists, throw error
+    if( file_exists(`./storage/images/${request_id}`) ) {
+        console.error('json file already exists');
+        // add the data to the request id in the json table
+        request_id = `${request_id}_${Date.now()}`;
+    }
     // if response is empty, dont save it
     if( await response.status() === 204 ) return;
+    // if request id is undefined, throw error
+    if( !request_id ){
+        console.log('headers: ', headers);
+        throw new Error('request id is undefined');
+    }else
+        // add the url to the request id in the json table
+        tables['json'][request_id] = url;
     // get the response
     const json = await response.json();
-    // make the directories
-    let file_path = _makeDirs(url, './storage/json/');
     // save the json
-    write_json(json, `./storage/json/${file_path}`);
-    console.log(`saved ./storage/json/${file_path}`);
+    write_json(json, `./storage/json/${request_id}.json`);
+    // save the json table
+    write_json(tables['json'], './storage/json_table.js');
+    console.log(`saved ./storage/json/${request_id}`);
 }
 
 // handle js code
 const handleJs = async (route, response, url) => {
+    // get headers
+    let headers = response.headers();
+    // get the request id
+    let request_id = headers['x-akamai-request-id'];
+    // if file exists, throw error
+    if( file_exists(`./storage/js/${request_id}`) ){
+        console.error('js file already exists');
+        // add date to file name
+        request_id = `${request_id}_${Date.now()}`;
+    }
+    // if request id is undefined, throw error
+    if( !request_id ){
+        console.log('headers: ', headers);
+        throw new Error('request id is undefined');
+    }else
+        // add the url to the request id in the js table
+        tables['js'][request_id] = url;
     // get the text
     let js_code = await response.text();
-    // make the directories
-    let file_path = _makeDirs(url, './storage/js/');
     //  save the js code
-    fs.writeFileSync(`./storage/js/${file_path}`, js_code);
-    console.log(`saved ./storage/js/'${file_path}`)
+    fs.writeFileSync(`./storage/js/${request_id}`, js_code);
+    // save the js table
+    write_json(tables['js'], './storage/js_table.js');
+    console.log(`saved ./storage/js/'${request_id}`)
 }
 
 // handle html code
 const handleHtml = async (route, response, url) => {
+    // get headers
+    let headers = response.headers();
+    // get the request id
+    let request_id = headers['x-akamai-request-id'];
+    // if file exists, throw error and add date to file name
+    if( file_exists(`./storage/html/${request_id}`) ){
+        console.error('html file already exists');
+        // add date to file name
+        request_id = `${request_id}_${Date.now()}`;
+    }
+    // if request id is undefined, throw error
+    if( !request_id ){
+        console.log('headers: ', headers);
+        throw new Error('request id is undefined');
+    }else
+        // add the url to the request id in the html table
+        tables['html'][request_id] = url;
     // get the text
     let html_code = await response.text();
-    // make the directories
-    let file_path = _makeDirs(url, './storage/html/');
     // save the html code
-    fs.writeFileSync(`./storage/html/${file_path}`, html_code);
-    console.log(`saved ./storage/html/${file_path}`)
+    fs.writeFileSync(`./storage/html/${request_id}`, html_code);
+    // save the html table
+    write_json(tables['html'], './storage/html_table.js');
+    console.log(`saved ./storage/html/${request_id}`)
 }
 
 const saveTraffic = async route => {
@@ -149,9 +228,11 @@ const saveTraffic = async route => {
     // handle response based on content type
     try{
         if( contentType === 'video/mp4'){
+            // save the video with by the request id
             await handleVideo(route, response, url);
-        }else if( contentType === 'application/json; charset=utf-8'){
-            // save the json through the url
+        }else if( contentType === 'application/json' 
+            || contentType === 'application/json; charset=utf-8'){
+            // save the json by the request id
             await handleJson(route, response, url);
         }else if( contentType === 'image/jpeg'){
             // save the image through the url
@@ -163,12 +244,12 @@ const saveTraffic = async route => {
         }else if( contentType === 'text/html; charset=utf-8'){
             // save the html code through the url
             await handleHtml(route, response, url)
-        }else
-            console.error('content type not handled: ' + contentType);
-    
+        }else{
+            //console.error('content type not handled: ' + contentType);
+        }
     }catch(err){
         console.log(err);
-        console.log('response: ', response);
+        //console.log('response: ', response);
     }
     // continue with the request, to edit request headers use route.fulfill({ response })
     await route.continue()
